5a5b3ee8f8224aa00b03cdaba10a95f07f1598df
Gabriel Mendes de Campos <87998234+GabrielMendesdc@users.noreply.github.com>
2021-12-01 13:07:29 -0300

    Add files via upload

--- USERS/urls.py
+++ USERS/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
 from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 from . import views
 
+
 app_name = 'users'
+template_name = 'users/login.html'
 
 urlpatterns = [
     #pagina de login
-    re_path(r'^login/$', login, {'template_name': 'users/login.html'},
-            name='login'),
+    re_path(r'^login/$', LoginView.as_view(), name = 'login'),
+
+    #pagina de logout
+    re_path(r'^logout/$', views.logout_view, name="logout"),
+    
+    #pagina de cadastro
+    re_path(r'^register/$', views.register, name='register'),
 ]

dfaa6134ead47dc271677c3f41679b29273a509d
Gabriel Mendes de Campos <87998234+GabrielMendesdc@users.noreply.github.com>
2021-12-04 13:47:04 -0300

    Add files via upload

--- DOZERO/settings.py
+++ DOZERO/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     #AS BRABA AGR
     'dozeros',
     'users',
@@ -128,3 +129,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/users/login/'
+
+BOOTSTRAP3 = {
+    'include_jquery': True,
+    }
